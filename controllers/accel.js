9ed7c725bfb4bff2019f94fa15d9ad610d75139e
Dr. Jonathan Bradley <bradley1@vt.edu>
2017-03-01 13:50:40 -0500

    Test moment.js integration with accel.js

--- controllers/accel.js
+++ controllers/accel.js
@@ -4,16 +4,16 @@ module.exports = {
     var timeSet = false
 
     accelerometer.on('change', function () {
-      var timeNow = new Date()
+      var timeNow = moment()
       if (this.acceleration > config.accelSensitivity && !timeSet) {
-        timeSet = new Date()
+        timeSet = moment()
         // console.log(beacon.getLocationName(beacon.closestBeacon))
         // console.log(battery.voltage)
         console.log('Accelerometer Triggered')
         request.post(config.postOptions, function (error, response, body) {
           console.log(response.statusCode)
-        }).form({device_name: config.deviceID, current_location: beacon.getLocationName(beacon.closestBeacon), time_of_movement: timeSet.toISOString().slice(0, 19).replace('T', ' '), battery_level: battery.voltage, alarm_state: alarm.alarmState})
-      } else if (timeNow - timeSet > config.accelBounce) {
+        }).form({device_name: config.deviceID, current_location: beacon.getLocationName(beacon.closestBeacon), time_of_movement: timeSet.local().format('YYYY-MM-DD HH:mm:ss'), battery_level: battery.voltage, alarm_state: alarm.alarmState})
+      } else if (timeNow.duration().subtract(timeSet).milliseconds() > config.accelBounce) {
         timeSet = false
       }
     })
