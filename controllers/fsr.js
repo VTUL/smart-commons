0642eee3effd471e1864d56e5e962ca2ddcba042
Dr. Jonathan Bradley <bradley1@vt.edu>
2017-03-01 11:23:05 -0500

    Format date string in js to match MySQL

--- controllers/fsr.js
+++ controllers/fsr.js
@@ -14,7 +14,7 @@ module.exports = {
         if (duration > 10000) {
           request.post(config.postOptions, function (error, response, body) {
             console.log(response.statusCode)
-          }).form({device_name: config.deviceID, current_location: beacon.getLocationName(beacon.closestBeacon), time_sat_down: timeSat, time_stood_up: timeStood, duration_of_sit: duration, battery_level: battery.voltage, alarm_state: alarm.alarmState})
+          }).form({device_name: config.deviceID, current_location: beacon.getLocationName(beacon.closestBeacon), time_sat_down: timeSat.toISOString().slice(0, 19).replace('T', ' ');, time_stood_up: timeStood.toISOString().slice(0, 19).replace('T', ' ');, duration_of_sit: duration, battery_level: battery.voltage, alarm_state: alarm.alarmState})
           console.log('FSR triggered')
         }
         timeSat = false
